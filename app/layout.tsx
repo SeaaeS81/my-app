95151c632f790e709dea2f3b82eca0efa84c56c9
SeaaeS81 <marlboro8129@gmail.com>
2025-06-17 15:19:32 +0200

    Task6: layout Header  footer

--- app/layout.tsx
+++ app/layout.tsx
@@ -2,8 +2,8 @@ import type { Metadata } from "next";
 import { Geist, Geist_Mono } from "next/font/google";
 import "./globals.css";
 
-import Header from "@/components/ui/Header";
-import Footer from "@/components/ui/footer";
+import Header from "@/components/Header";
+import Footer from "@/components/footer";
 //import { Table } from "@/components/ui/table";  // именованный импорт
 
 const geistSans = Geist({
