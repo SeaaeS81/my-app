301bf83bf8978cbca9e4a9746646f4850055fead
SeaaeS81 <marlboro8129@gmail.com>
2025-06-17 13:29:56 +0200

    Task5 cpmponents Header footer

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,8 +1,8 @@
 import type { Metadata } from "next";
 import { Geist, Geist_Mono } from "next/font/google";
 import "./globals.css";
-import Header from "@/components/Header"; // Убедись, что файл называется с заглавной буквы
-import Footer from "@/components/footer"; // Тоже с заглавной, если файл `Footer.tsx`
+import Header from "@/components/Header"; 
+import Footer from "@/components/footer"; 
 
 const geistSans = Geist({
   variable: "--font-geist-sans",
